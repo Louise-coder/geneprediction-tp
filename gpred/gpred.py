5373a9c29726cd3912c0f6fd40a08701f1f6f976
Louise <louloulam1606@gmail.com>
2024-10-14 11:24:23 +0200

    Black formatting the code.

--- gpred/gpred.py
+++ gpred/gpred.py
@@ -28,30 +28,58 @@ def isfile(path: str) -> Path:  # pragma: no cover
     return myfile
 
 
-def get_arguments(): # pragma: no cover
+def get_arguments():  # pragma: no cover
     """Retrieves the arguments of the program.
 
     :return: An object that contains the arguments
     """
     # Parsing arguments
-    parser = argparse.ArgumentParser(description=__doc__, usage=
-                                     "{0} -h"
-                                     .format(sys.argv[0]))
-    parser.add_argument('-i', dest='genome_file', type=isfile, required=True, 
-                        help="Complete genome file in fasta format")
-    parser.add_argument('-g', dest='min_gene_len', type=int, 
-                        default=50, help="Minimum gene length to consider (default 50).")
-    parser.add_argument('-s', dest='max_shine_dalgarno_distance', type=int, 
-                        default=16, help="Maximum distance from start codon "
-                        "where to look for a Shine-Dalgarno motif (default 16).")
-    parser.add_argument('-d', dest='min_gap', type=int, default=40,
-                        help="Minimum gap between two genes - shine box not included (default 40).")
-    parser.add_argument('-p', dest='predicted_genes_file', type=Path, 
-                        default=Path("predict_genes.csv"),
-                        help="Tabular file giving position of predicted genes")
-    parser.add_argument('-o', dest='fasta_file', type=Path,
-                        default=Path("genes.fna"),
-                        help="Fasta file giving sequence of predicted genes")
+    parser = argparse.ArgumentParser(
+        description=__doc__, usage="{0} -h".format(sys.argv[0])
+    )
+    parser.add_argument(
+        "-i",
+        dest="genome_file",
+        type=isfile,
+        required=True,
+        help="Complete genome file in fasta format",
+    )
+    parser.add_argument(
+        "-g",
+        dest="min_gene_len",
+        type=int,
+        default=50,
+        help="Minimum gene length to consider (default 50).",
+    )
+    parser.add_argument(
+        "-s",
+        dest="max_shine_dalgarno_distance",
+        type=int,
+        default=16,
+        help="Maximum distance from start codon "
+        "where to look for a Shine-Dalgarno motif (default 16).",
+    )
+    parser.add_argument(
+        "-d",
+        dest="min_gap",
+        type=int,
+        default=40,
+        help="Minimum gap between two genes - shine box not included (default 40).",
+    )
+    parser.add_argument(
+        "-p",
+        dest="predicted_genes_file",
+        type=Path,
+        default=Path("predict_genes.csv"),
+        help="Tabular file giving position of predicted genes",
+    )
+    parser.add_argument(
+        "-o",
+        dest="fasta_file",
+        type=Path,
+        default=Path("genes.fna"),
+        help="Fasta file giving sequence of predicted genes",
+    )
     return parser.parse_args()
 
 
@@ -59,35 +87,44 @@ def read_fasta(fasta_file: Path) -> str:
     """Extract genome sequence from fasta files.
 
     :param fasta_file: (Path) Path to the fasta file.
-    :return: (str) Sequence from the genome. 
+    :return: (str) Sequence from the genome.
     """
     pass
 
 
-def find_start(start_regex: Pattern, sequence: str, start: int, stop: int) -> Union[int, None]:
+def find_start(
+    start_regex: Pattern, sequence: str, start: int, stop: int
+) -> Union[int, None]:
     """Find next start codon before a end position.
 
     :param start_regexp: A regex object that identifies a start codon.
     :param sequence: (str) Sequence from the genome
     :param start: (int) Start position of the research
     :param stop: (int) Stop position of the research
-    :return: (int) If exist, position of the start codon. Otherwise None. 
+    :return: (int) If exist, position of the start codon. Otherwise None.
     """
     pass
 
 
-def find_stop(stop_regex: Pattern, sequence: str, start: int) -> Union[int, None]:
+def find_stop(
+    stop_regex: Pattern, sequence: str, start: int
+) -> Union[int, None]:
     """Find next stop codon that should be in the same reading phase as the start.
 
     :param stop_regexp: A regex object that identifies a stop codon.
     :param sequence: (str) Sequence from the genome
     :param start: (int) Start position of the research
-    :return: (int) If exist, position of the stop codon. Otherwise None. 
+    :return: (int) If exist, position of the stop codon. Otherwise None.
     """
     pass
 
 
-def has_shine_dalgarno(shine_regex: Pattern, sequence: str, start: int, max_shine_dalgarno_distance: int) -> bool:
+def has_shine_dalgarno(
+    shine_regex: Pattern,
+    sequence: str,
+    start: int,
+    max_shine_dalgarno_distance: int,
+) -> bool:
     """Find a shine dalgarno motif before the start codon
 
     :param shine_regexp: A regex object that identifies a shine-dalgarno motif.
@@ -130,8 +167,13 @@ def write_genes_pos(predicted_genes_file: Path, probable_genes: List[List[int]])
         sys.exit("Error cannot open {}".format(predicted_genes_file))
 
 
-def write_genes(fasta_file: Path, sequence: str, probable_genes: List[List[int]], sequence_rc: str, 
-                probable_genes_comp: List[List[int]]):
+def write_genes(
+    fasta_file: Path,
+    sequence: str,
+    probable_genes: List[List[int]],
+    sequence_rc: str,
+    probable_genes_comp: List[List[int]],
+):
     """Write gene sequence in fasta format
 
     :param fasta_file: (Path) Output fasta file.
@@ -142,15 +184,23 @@ def write_genes(fasta_file: Path, sequence: str, probable_genes: List[List[int]]
     """
     try:
         with open(fasta_file, "wt") as fasta:
-            for i,gene_pos in enumerate(probable_genes):
-                fasta.write(">gene_{0}{1}{2}{1}".format(
-                    i+1, os.linesep, 
-                    fill(sequence[gene_pos[0]-1:gene_pos[1]])))
-            i = i+1
-            for j,gene_pos in enumerate(probable_genes_comp):
-                fasta.write(">gene_{0}{1}{2}{1}".format(
-                            i+1+j, os.linesep,
-                            fill(sequence_rc[gene_pos[0]-1:gene_pos[1]])))
+            for i, gene_pos in enumerate(probable_genes):
+                fasta.write(
+                    ">gene_{0}{1}{2}{1}".format(
+                        i + 1,
+                        os.linesep,
+                        fill(sequence[gene_pos[0] - 1 : gene_pos[1]]),
+                    )
+                )
+            i = i + 1
+            for j, gene_pos in enumerate(probable_genes_comp):
+                fasta.write(
+                    ">gene_{0}{1}{2}{1}".format(
+                        i + 1 + j,
+                        os.linesep,
+                        fill(sequence_rc[gene_pos[0] - 1 : gene_pos[1]]),
+                    )
+                )
     except IOError:
         sys.exit("Error cannot open {}".format(fasta_file))
 
@@ -161,26 +211,26 @@ def reverse_complement(sequence: str) -> str:
     :param sequence: (str) DNA Sequence.
     :return: (str) Reverse complemented sequence.
     """
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-    return ''.join([complement[base] for base in sequence[::-1]])
+    complement = {"A": "T", "C": "G", "G": "C", "T": "A"}
+    return "".join([complement[base] for base in sequence[::-1]])
 
 
-#==============================================================
+# ==============================================================
 # Main program
-#==============================================================
-def main() -> None: # pragma: no cover
+# ==============================================================
+def main() -> None:  # pragma: no cover
     """
     Main program function
     """
     # Gene detection over genome involves to consider a thymine instead of
     # an uracile that we would find on the expressed RNA
-    #start_codons = ['TTG', 'CTG', 'ATT', 'ATG', 'GTG']
-    #stop_codons = ['TAA', 'TAG', 'TGA']
-    start_regex = re.compile('AT[TG]|[ATCG]TG')
-    stop_regex = re.compile('TA[GA]|TGA')
+    # start_codons = ['TTG', 'CTG', 'ATT', 'ATG', 'GTG']
+    # stop_codons = ['TAA', 'TAG', 'TGA']
+    start_regex = re.compile("AT[TG]|[ATCG]TG")
+    stop_regex = re.compile("TA[GA]|TGA")
     # Shine AGGAGGUAA
-    #AGGA ou GGAGG 
-    shine_regex = re.compile('A?G?GAGG|GGAG|GG.{1}GG')
+    # AGGA ou GGAGG
+    shine_regex = re.compile("A?G?GAGG|GGAG|GG.{1}GG")
     # Arguments
     args = get_arguments()
     # Let us do magic in 5' to 3'
@@ -195,5 +245,5 @@ def main() -> None: # pragma: no cover
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
